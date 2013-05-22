9d47bc89668b6e24f213e35886b2efcf12ab573d
Adam Demuri <adam.demuri@gmail.com>
2013-05-22 00:18:32 -0500

    Added plugins to BuildConfig, best practice is to keep them here

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,21 +43,23 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-//        runtime ":jquery:1.7.2"
-//        runtime ":resources:1.1.6"
-		//runtime "com.mysql.jdbc.Driver"
-
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-//        runtime ":zipped-resources:1.0"
-//        runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
 
         build ":tomcat:$grailsVersion"
 		
-
-//        runtime ":database-migration:1.1"
-
-//		compile "org.codehaus.gpars:gpars:0.12"
-//        compile ':cache:1.0.0'
+		compile ':cache:1.0.1'
+		compile ':cache-headers:1.1.5'
+		compile ':cached-resources:1.0'
+		compile ':form-helper:0.2.8'
+		compile ':hibernate-stats:1.1'
+		compile ':jquery:1.7.2'
+		compile ':jquery-ui:1.8.7'
+		compile ':pretty-time:2.1.3.Final-1.0.1'
+		compile ':quartz:1.0-RC7'
+		compile ':quartz-monitor:0.3-RC1'
+		compile ':resources:1.2.RC2'
+		compile ':rest:0.7'
+		compile ':webxml:1.4.1'
+		compile ':yui-minify-resources:0.1.5'
+		compile ':zipped-resources:1.0'
     }
 }
